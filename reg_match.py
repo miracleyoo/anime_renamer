d05f184d55390992e28a43b65cf836a85cbd65eb
Miracleyoo <miracleyoo@163.com>
2021-01-31 15:05:47 -0500

    fix bug

--- reg_match.py
+++ reg_match.py
@@ -1,7 +1,10 @@
 import re
 import os
+import io
+import sys
 import os.path as op
 import argparse
+import codecs
 from glob import glob
 from pathlib2 import Path
 
@@ -17,6 +20,7 @@ episode_rules = [r'(.*)\[(\d{1,3}|\d{1,3}\.\d{1,2})(?:v\d{1,2})?(?:END)?\](.*)',
                  r'(.*)- (\d{1,3}|\d{1,3}\.\d{1,2})(?:v\d{1,2})?(?:END)? (.*)']
 # Suffixs of files we are going to rename
 suffixs = ['mp4', 'mkv', 'avi', 'mov', 'flv', 'rmvb', 'ass', 'idx']
+sys.stdout = io.TextIOWrapper(buffer=sys.stdout.buffer,encoding='utf8')
 
 # Parse the input arguments. You can whether input only root, or only path, or both root and name.
 parser = argparse.ArgumentParser(description='Regular Expression Match')
@@ -38,11 +42,13 @@ def rename(root, name):
             # print(matchObj.group())
             # print(new_name)
             print(f'{name} -> {new_name}')
-            with open(log_name, 'a+') as f:
-                print(f'{name} -> {new_name}', file=f)
+            with codecs.open(log_name, 'a+', 'utf-8') as f:
+                f.writelines(f'{name} -> {new_name}')
+                # print(f'{name} -> {new_name}', file=f)
 
             os.rename(str(root/name), str(root/new_name))
-            break
+            general_check(root, new_name)
+            return
     general_check(root, name)
 
 
@@ -50,7 +56,7 @@ def general_check(root, name):
     new_name = ' '.join(name.split())
     if new_name != name:
         print(f'{name} -> {new_name}')
-        with open(log_name, 'a+') as f:
+        with codecs.open(log_name, 'a+', 'utf-8') as f:
             print(f'{name} -> {new_name}', file=f)
         os.rename(str(root/name), str(root/new_name))
 
@@ -60,6 +66,12 @@ if __name__ == "__main__":
     if op.isdir(args.path):
         args.root = args.path
         args.path = ''
+
+    if args.name != '' and args.root != '':
+        temp = str(args.root/args.name)
+        if op.isdir(temp):
+            args.root = temp
+            args.name = ''
 
     if args.path != '':
         root, name = op.split(args.path)
@@ -71,14 +83,13 @@ if __name__ == "__main__":
         for suffix in suffixs:
             files.extend(Path(args.root).rglob('*.'+suffix))
             files.extend(Path(args.root).rglob('*.'+suffix.upper()))
-
         print(f'Total Files Number: {len(files)}')
         for path in files:
             root, name = op.split(path)
             rename(root, name)
     else:
         print('Please input whether only root, or only path, or both root and name')
-    os.system('PAUSE')
+    # os.system('PAUSE')
     # for rule in episode_rules:
     #     matchObj = re.match(rule, name, re.I)
     #     if matchObj is not None:
